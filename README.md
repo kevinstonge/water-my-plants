d07803224510e8500ee6b609fc65484c98707ed5
Kevin StOnge <kevinstonge@gmail.com>
2020-12-16 11:24:45 -0500

    added dotenv to knexfile

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 Frontend repo: https://github.com/ialkamal/water-plants
 
 Backend repo: https://github.com/kevinstonge/water-my-plants
+
+Backend Heroku App: https://water-my-plants-lambda.herokuapp.com
+
+Endpoints:
+
+POST to /api/register { username, phone, password }
+
+POST to /api/login { username, password }
--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
 // Update with your config settings.
-
+require("dotenv").config();
 module.exports = {
   development: {
     client: "sqlite3",
@@ -24,7 +24,6 @@ module.exports = {
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
-    migrations: { directory: "./data/migrations" },
-    ssl: true,
+    migrations: { directory: __dirname + "./data/migrations" },
   },
 };
