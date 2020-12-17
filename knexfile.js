97be13c1d44bc2740b822bec4fdeaf3dd0ade5e2
Kevin St.Onge <kevinstonge@gmail.com>
2020-12-17 18:04:06 -0500

    ...

--- knexfile.js
+++ knexfile.js
@@ -23,14 +23,7 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: {
-      database: process.env.DATABASE_URL,
-      ssl: true
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    connection: `${process.env.DATABASE_URL}?ssl=true`,
     migrations: { directory: "./data/migrations" },
   },
 };
