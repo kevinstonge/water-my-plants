4faa0d3ba5c098589861ec2bf4e4fbcad5490fe5
Kevin St.Onge <kevinstonge@gmail.com>
2020-12-16 21:14:49 -0500

    idk

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
 // Update with your config settings.
-require("dotenv").config();
+
 module.exports = {
   development: {
     client: "sqlite3",
@@ -24,6 +24,10 @@ module.exports = {
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
     migrations: { directory: "./data/migrations" },
   },
 };
